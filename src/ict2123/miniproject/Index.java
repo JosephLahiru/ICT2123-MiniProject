b59bc9b113f84af5f943e7a0701d427d003e759b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-05-25 15:28:32 +0530

    Update Index.java

--- src/ict2123/miniproject/Index.java
+++ src/ict2123/miniproject/Index.java
@@ -16,6 +16,7 @@ public class Index extends javax.swing.JFrame {
     public Index() {
         initComponents();
         setLocationRelativeTo(null);
+        jButton1.setFocusable(false);
     }
 
     /**
